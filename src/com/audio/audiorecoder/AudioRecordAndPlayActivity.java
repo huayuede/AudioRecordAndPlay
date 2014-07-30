8887695108a0773508bbf34fb52b71785fb2ebe3
wusy <wusy3@lenovo.com>
2014-07-30 09:21:04 +0800

    add thread stop

--- src/com/audio/audiorecoder/AudioRecordAndPlayActivity.java
+++ src/com/audio/audiorecoder/AudioRecordAndPlayActivity.java
@@ -22,8 +22,8 @@ import android.widget.Toast;
 public class AudioRecordAndPlayActivity extends Activity {
 	private static final String TAG = "AudioRecordAndPlayActivity";
 	private static final int SAMPLE_RATE = 44100;
-	private static final int QUEUE_SIZE = 16; 
-		
+	private static final int QUEUE_SIZE = 16;
+
 	private AudioRecord mAudioRecord;
 	private AudioTrack mAudioTrack;
 	private AcousticEchoCanceler mAEC;
@@ -34,9 +34,12 @@ public class AudioRecordAndPlayActivity extends Activity {
 	private int mAudioOutBufferSize;
 	private boolean mRecordAndPlay = false;
 	private boolean mRecordAndPlayReserved = false;
+	private volatile boolean mRecordThreadStop = false;
+	private volatile boolean mPlayThreadStop = false;
 	private Thread mRecordSoundThread;
 	private Thread mPlaySoundThread;
 	private Button mButton;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -45,13 +48,13 @@ public class AudioRecordAndPlayActivity extends Activity {
 		this.setTitle(TAG);
 		init();
 		Log.i(TAG, "OnCreate init done");
-		
+
 		mRecordSoundThread = new Thread(new RecordSound());
 		mPlaySoundThread = new Thread(new PlaySound());
 		mRecordSoundThread.start();
 		mPlaySoundThread.start();
 	}
-	
+
 	@Override
 	protected void onPause() {
 		super.onPause();
@@ -59,17 +62,19 @@ public class AudioRecordAndPlayActivity extends Activity {
 		mRecordAndPlayReserved = mRecordAndPlay;
 		mRecordAndPlay = false;
 	}
-	
+
 	@Override
 	protected void onResume() {
 		super.onResume();
 		Log.i(TAG, "onResume");
 		mRecordAndPlay = mRecordAndPlayReserved;
 	}
-	
+
 	@Override
 	protected void onStop() {
 		super.onStop();
+		mRecordThreadStop = true;
+		mPlayThreadStop = true;
 		Log.i(TAG, "onStop");
 		if (mAudioRecord != null) {
 			mAudioRecord.stop();
@@ -87,17 +92,18 @@ public class AudioRecordAndPlayActivity extends Activity {
 			mAEC = null;
 		}
 	}
-	
+
 	@Override
 	protected void onStart() {
 		super.onStart();
 		Log.i(TAG, "onStart");
 	}
-	
-	@Override 
+
+	@Override
 	protected void onDestroy() {
 		super.onDestroy();
 		Log.i(TAG, "onDestroy");
+
 	}
 
 	@Override
@@ -125,11 +131,13 @@ public class AudioRecordAndPlayActivity extends Activity {
 			public void onClick(View v) {
 				if (mRecordAndPlay == false) {
 					mRecordAndPlay = true;
-					Toast.makeText(AudioRecordAndPlayActivity.this, "Start record and play", Toast.LENGTH_SHORT).show();
+					Toast.makeText(AudioRecordAndPlayActivity.this,
+							"Start record and play", Toast.LENGTH_SHORT).show();
 					mButton.setText("Stop");
 				} else {
 					mRecordAndPlay = false;
-					Toast.makeText(AudioRecordAndPlayActivity.this, "Stop record and play", Toast.LENGTH_SHORT).show();
+					Toast.makeText(AudioRecordAndPlayActivity.this,
+							"Stop record and play", Toast.LENGTH_SHORT).show();
 					mButton.setText("Start");
 				}
 			}
@@ -139,20 +147,23 @@ public class AudioRecordAndPlayActivity extends Activity {
 				AudioFormat.CHANNEL_IN_STEREO, AudioFormat.ENCODING_PCM_16BIT);
 		mAudioInBuffer = new byte[mAudioInBufferSize];
 		if (android.os.Build.VERSION.SDK_INT >= 16) {
-			mAudioRecord = new AudioRecord(MediaRecorder.AudioSource.VOICE_COMMUNICATION,
-					SAMPLE_RATE, AudioFormat.CHANNEL_IN_STEREO,
+			mAudioRecord = new AudioRecord(
+					MediaRecorder.AudioSource.VOICE_COMMUNICATION, SAMPLE_RATE,
+					AudioFormat.CHANNEL_IN_STEREO,
 					AudioFormat.ENCODING_PCM_16BIT, mAudioInBufferSize);
 			if (AcousticEchoCanceler.isAvailable()) {
-				mAEC = AcousticEchoCanceler.create(mAudioRecord.getAudioSessionId());
+				mAEC = AcousticEchoCanceler.create(mAudioRecord
+						.getAudioSessionId());
 				try {
 					mAEC.setEnabled(true);
-		        } catch (IllegalStateException e) {
-		        	e.printStackTrace();
-		        }
+				} catch (IllegalStateException e) {
+					e.printStackTrace();
+				}
 			}
 		} else {
-			mAudioRecord = new AudioRecord(MediaRecorder.AudioSource.VOICE_COMMUNICATION,
-					SAMPLE_RATE, AudioFormat.CHANNEL_IN_STEREO,
+			mAudioRecord = new AudioRecord(
+					MediaRecorder.AudioSource.VOICE_COMMUNICATION, SAMPLE_RATE,
+					AudioFormat.CHANNEL_IN_STEREO,
 					AudioFormat.ENCODING_PCM_16BIT, mAudioInBufferSize);
 		}
 
@@ -160,52 +171,59 @@ public class AudioRecordAndPlayActivity extends Activity {
 				AudioFormat.CHANNEL_OUT_STEREO, AudioFormat.ENCODING_PCM_16BIT);
 		mAudioOutBuffer = new byte[mAudioOutBufferSize];
 		if (android.os.Build.VERSION.SDK_INT >= 16) {
-			mAudioTrack = new AudioTrack(AudioManager.STREAM_MUSIC, 44100,
-					AudioFormat.CHANNEL_OUT_STEREO, AudioFormat.ENCODING_PCM_16BIT,
-					mAudioOutBufferSize, AudioTrack.MODE_STREAM, mAudioRecord.getAudioSessionId());
+			mAudioTrack = new AudioTrack(AudioManager.STREAM_VOICE_CALL, 44100,
+					AudioFormat.CHANNEL_OUT_STEREO,
+					AudioFormat.ENCODING_PCM_16BIT, mAudioOutBufferSize,
+					AudioTrack.MODE_STREAM, mAudioRecord.getAudioSessionId());
 		} else {
-			mAudioTrack = new AudioTrack(AudioManager.STREAM_MUSIC, 44100,
-					AudioFormat.CHANNEL_OUT_STEREO, AudioFormat.ENCODING_PCM_16BIT,
-					mAudioOutBufferSize, AudioTrack.MODE_STREAM);
+			mAudioTrack = new AudioTrack(AudioManager.STREAM_VOICE_CALL, 44100,
+					AudioFormat.CHANNEL_OUT_STEREO,
+					AudioFormat.ENCODING_PCM_16BIT, mAudioOutBufferSize,
+					AudioTrack.MODE_STREAM);
 		}
-		
+
 		mAudioDataQueue = new ArrayBlockingQueue<byte[]>(QUEUE_SIZE);
 	}
-	
+
 	private class RecordSound implements Runnable {
 		@Override
 		public void run() {
 			mAudioRecord.startRecording();
 			byte[] tmpBuffer;
-			while(true) {
+			while (!mRecordThreadStop) {
 				if (mRecordAndPlay == true) {
-					mAudioRecord.read(mAudioInBuffer, 0, mAudioInBufferSize);
-					tmpBuffer = mAudioInBuffer.clone();
-					Log.d(TAG, "Record data length" + tmpBuffer.length);
-					try {
-						mAudioDataQueue.put(tmpBuffer);
-					} catch (InterruptedException e) {
-						e.printStackTrace();
+					if (mAudioRecord != null) {
+						mAudioRecord
+								.read(mAudioInBuffer, 0, mAudioInBufferSize);
+						tmpBuffer = mAudioInBuffer.clone();
+						Log.d(TAG, "Record data length" + tmpBuffer.length);
+						try {
+							mAudioDataQueue.put(tmpBuffer);
+						} catch (InterruptedException e) {
+							e.printStackTrace();
+						}
 					}
 				}
 			}
 		}
 	}
-	
+
 	private class PlaySound implements Runnable {
 		@Override
 		public void run() {
 			mAudioTrack.play();
 			byte[] tmpBuffer;
-			while(true) {
+			while (!mPlayThreadStop) {
 				if (mRecordAndPlay == true) {
-					try {
-						mAudioOutBuffer = mAudioDataQueue.take();
-						tmpBuffer = mAudioOutBuffer.clone();
-						Log.d(TAG, "Play data length" + tmpBuffer.length);
-						mAudioTrack.write(tmpBuffer, 0, tmpBuffer.length);
-					} catch (InterruptedException e) {
-						e.printStackTrace();
+					if (mAudioTrack != null) {
+						try {
+							mAudioOutBuffer = mAudioDataQueue.take();
+							tmpBuffer = mAudioOutBuffer.clone();
+							Log.d(TAG, "Play data length" + tmpBuffer.length);
+							mAudioTrack.write(tmpBuffer, 0, tmpBuffer.length);
+						} catch (InterruptedException e) {
+							e.printStackTrace();
+						}
 					}
 				}
 			}
